dcfbdfb874f351b44a70959d39fe174da0024c85
Neha-singh18 <singhrajputneha896@gmail.com>
2025-04-14 10:52:18 +0530

    Implement user profile fetching and enhance Navbar with user greeting

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { USER } from "../utils/constants";
+import {addUser} from "../utils/userSlice";
+// import { useNavigate } from "react-router-dom";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    if(user) return ;
+    try {
+      const response = await axios.get(USER + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
